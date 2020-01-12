250828a64417388f54a643ae096827b44904fab1
y.benkhallouf-ext <y.benkhallouf-ext@lefebvre-sarrut.eu>
2020-01-12 20:41:56 +0100

    add cartIcon cartDropdown componenents

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
     return (
         <div className='header'>
             <Link className='logo-container' to='/'>
@@ -23,17 +26,20 @@ const Header = ({ currentUser }) => {
                         SIGN OUT
                     </div>
                 ) : (
-                <Link className='option' to='/signin'>
+                        <Link className='option' to='/signin'>
                             SIGN IN
                 </Link>
                     )}
+                <CartIcon />
             </div>
+            {hidden ? null : <CartDropDown />}
         </div>
     )
 
 }
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 })
-export default connect (mapStateToProps) (Header);
+export default connect(mapStateToProps)(Header);
